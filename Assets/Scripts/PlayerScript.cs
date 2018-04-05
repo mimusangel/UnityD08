5df51e31e4ebc52a9e411d3d30501f1ea0766e7d
Mimus Angel <mgallo@student.42.fr>
2018-04-05 21:25:07 +0200

    Camera and Player base Script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Component
+	UnityEngine.AI.NavMeshAgent agent;
+	Animator					animator;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Debug.DrawRay(mouseRay.origin, mouseRay.direction, Color.red);
+			if (Physics.Raycast(mouseRay, out hit))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+		if (agent.velocity.magnitude >= 0.2f)
+			animator.SetBool("run", true);
+		else
+			animator.SetBool("run", false);
+	}
+}
---
+++ Assets/Scripts/ThirdPersonCameraScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraScript : MonoBehaviour {
+
+	GameObject	player;
+	Vector2		angle;
+	float		distance = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		angle = new Vector2(45, -45);
+		//Quaternion.AngleAxis(angle.x, Vector3.right);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.R))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			angle.y += Input.GetAxis("Mouse X");
+			angle.x += Input.GetAxis("Mouse Y");
+			angle.x = Mathf.Clamp(angle.x, 20.0f, 89.0f);
+		}
+
+		distance += Input.GetAxis("Mouse ScrollWheel");
+		distance = Mathf.Clamp(distance, 6.0f, 15.0f);
+
+		if (Input.GetKeyUp(KeyCode.R))
+			Cursor.lockState = CursorLockMode.None;
+
+		transform.rotation = Quaternion.Euler(angle.x, angle.y, 0);
+
+		RaycastHit hit;
+		if(Physics.Raycast(player.transform.position, transform.forward * -1, out hit, distance))
+			transform.position = hit.point;
+		else
+			transform.position = player.transform.position + transform.forward * -distance;
+
+	}
+}
