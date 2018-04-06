16b22527f4e8874625e406434318540e0b4c9208
Marc-antoine GALLO <mgallo@e2r7p10.42.fr>
2018-04-06 08:58:26 +0200

    Maya IDLE Correctly and enemy animation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour {
 	// Component
 	UnityEngine.AI.NavMeshAgent agent;
 	Animator					animator;
+	bool						attack;
 
 	// Use this for initialization
 	void Start () {
@@ -16,19 +17,48 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(0))
+		if (animator.GetBool("attack") == false) // Peu se deplacer et/ou attaquer si il n'y a pas d'annimation d'attack en cour
 		{
-			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			Debug.DrawRay(mouseRay.origin, mouseRay.direction, Color.red);
-			if (Physics.Raycast(mouseRay, out hit))
+			if (Input.GetMouseButton(0))
 			{
-				agent.SetDestination(hit.point);
+				Move();
+				attack = false;
+			}
+			if (Input.GetMouseButtonDown(1))
+			{
+				Move();
+				attack = true;
 			}
 		}
-		if (agent.velocity.magnitude >= 0.2f)
-			animator.SetBool("run", true);
-		else
-			animator.SetBool("run", false);
+		// Debug.Log("Velocity: " + agent.velocity.magnitude);
+		bool endMove = pathComplete();
+		animator.SetBool("run", !endMove);
+		if (endMove && attack && agent.velocity.magnitude <= 0.0f)
+		{
+			animator.SetTrigger("attack");
+			attack = false;
+		}
+	}
+
+	void Move()
+	{
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		Debug.DrawRay(mouseRay.origin, mouseRay.direction, Color.red);
+		if (Physics.Raycast(mouseRay, out hit))
+		{
+			agent.SetDestination(hit.point);
+		}
+	}
+	protected bool pathComplete()
+	{
+		if (Vector3.Distance(agent.destination, agent.transform.position) <= agent.stoppingDistance)
+		{
+			if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+			{
+				return (true);
+			}
+		}
+		return (false);
 	}
 }
